b5b0ad53a323ca41d317520ee21b3da848a71bea
DusanBojanic <52867608+Teodorant1@users.noreply.github.com>
2022-04-04 12:50:43 +0200

    123
    123

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App (props) {
 
   const [paloki, setpaloki] = useState("LAWL1");
   const [delimiter, setdelimiter] = useState("spergzilion");
-  const [ipAddress, setipAddress] = useState('http://45.77.202.154:8001');
+  const [ipAddress, setipAddress] = useState('http://167.71.79.230:8001');
  
 
 
