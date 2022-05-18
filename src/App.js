da17bdbdd1914a41fc33abf848a6d0fe96b9182c
DusanBojanic <52867608+Teodorant1@users.noreply.github.com>
2022-05-18 23:00:37 +0200

    made the game sexier
    made the game extra sexier

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
 import react, {component} from 'react';
+import {bootstrap} from 'bootstrap'
 import axios, { Axios } from 'axios';
 import './App.css';
 import React from 'react';
 import { useState } from 'react';
+import {Button} from "bootstrap";
+import {Answer} from './answer'
+import "bootstrap/dist/css/bootstrap.min.css"
 
 
 
@@ -25,18 +28,26 @@ function App (props) {
  
   const [expansion, setexpansion] = useState("all");
 
-  const [paloki, setpaloki] = useState("LAWL1");
+  const [paloki, setpaloki] = useState("invisible");
   const [delimiter, setdelimiter] = useState("spergzilion");
   const [ipAddress, setipAddress] = useState('http://localhost:8001');
-  const [derp, setderp] = useState(scores.split(" "))
+  
+  const [page, setpage] = useState("instructions");
+  
   const [judge, setjudge] = useState(" The name of judge will appear here")
+
+
   const [players, setplayers] = useState(
-    [  {"name" : "Names will go here"  ,
-    "points"      : "Scores will go here" ,  
-    "response"     : "Answers will go here"} 
+    [  {"name"    : "Names will go here"  ,
+        "points"  : "Scores will go here" ,  
+        "response": "Answers will go here"} 
           ]
         );
   
+ 
+
+
+
 
   function Row ( {textorio , textorio1 } )
   {  return (<div>  {textorio} {textorio1}  <button onClick={handleVote} type="submit" class="btn btn-primary">Paloki</button>  </div> ) }
@@ -119,11 +130,11 @@ axios.post(ipAddress,votemessage).then(
 
   }
   function handleAnswer(event){
-  event.preventDefault();
+    
+    console.log("handleanswer")
+    event.preventDefault();
 
   const answer = document.getElementById('Answer').value;
-
-  setpaloki();
   const answermessage = 'existinggame'+delimiter+
 'answer'+delimiter+gameid+delimiter+playername+delimiter
 +answer+delimiter+"payload2"+delimiter+playerpassword
@@ -134,7 +145,7 @@ axios.post(ipAddress,votemessage).then(
 axios.post(ipAddress,answermessage).then( 
          
   (resp) => {
-  //  console.log(resp.data);
+   console.log(resp.data);
   }
  
 )
@@ -145,6 +156,8 @@ axios.post(ipAddress,answermessage).then(
 
  function handlejOin  (event) {
   event.preventDefault();
+setpage("1");
+setpaloki("visible");
 
 const joinmessage = 'existinggame'+delimiter+
 'addplayer'+delimiter+gameid+delimiter+playername+delimiter
@@ -159,11 +172,13 @@ axios.post(ipAddress,joinmessage).then(
 )
 .catch(error=> console.log(error) );
 
-setInterval(()=> { handleupdate() }, 1000 ) 
+setInterval(()=> { handleupdate() }, 15000 ) 
 }
 
 function handlecReate(event){ 
   event.preventDefault();
+  setpage("1");
+  setpaloki("visible");
 
   const createmessage = "creategame"+delimiter+gameid
   +delimiter+gamepassword+delimiter+expansion+delimiter
@@ -176,11 +191,11 @@ function handlecReate(event){
    
   )
   .catch(error=> console.log(error) );
-  setInterval(()=> { handleupdate() }, 1000 ) 
+  setInterval(()=> { handleupdate() }, 15000 ) 
 }
 //////////////////
 const handleupdate = async() =>
-{setderp(scores.split(" "));
+{
 
 
 
@@ -266,147 +281,145 @@ function handlepaloki1 (event)
      )
      .catch(error=> console.log(error) )
 }
+  function Instructionz ()
+  {  return <div class="p-3 mb-2 bg-info text-white">
+  <div class="flex-container" ><h1> Welcome to Teodorant Insanity Topic Simulator, this game is inspired by Forks in the timeline, so it basically plays like that minus the bonus cards.
+    
+    Rules: https://www.boardgamecapital.com/game_rules/forks-in-the-timeline.pdf</h1></div>
+
+
+     <div class="flex-container" > <button onClick={ ()=>{setpage("0")} } type="submit" class="btn btn-danger">Proceed To Setup</button> </div>
+    
+
+     <div class="flex-container" ><img src='https://i.imgur.com/DNAXf01.png' class="img-fluid" alt="Responsive image" /></div>
+    
+</div> }
+
+
+function Display () 
+{  
+  if (page === "instructions" ) 
+{  return  <div><Instructionz/> </div> } 
+
+  if (page=== "0")
+{  return <div class="p-3 mb-2 bg-info text-white" >
+  <div class="flex-container">  <h1> Teodorant Insanity Topic Simulator </h1>     
+     </div>
+
+     <div class="flex-container">  <img src='https://i.imgur.com/91gg3HR.png'/>  
+     </div>
+         
   
+ <div>  
+
+ 
+ <div class="flex-container">  
+ <form>
+<div   class="form-group">
+<h1> Join/Create Game </h1>
+
+
+<button 
+onClick={handlesetPlayerInfo}
+type="submit" class="btn btn-primary">Set Player and Game info</button>
+<button 
+onClick={handlejOin}
+type="submit" class="btn btn-primary">Join game</button>
+<button 
+onClick={handlecReate}
+type="submit" class="btn btn-primary">Create game</button>
+<button 
+onClick={handlepaloki1}
+type="submit" class="btn btn-primary">Super Secret Action</button>
+
+<div><label htmlFor="GAMEID"><h1>GAMEID</h1></label>
+ 
+<input type="password" className="form-control" id="GAMEID" placeholder="Game ID" />
+ </div>
+
+
+
+<div class="form-group">
+<label htmlFor="Game Password"><h1>Game Password</h1></label>
+<input type="password" className="form-control" id="Game Password" placeholder="Game Password"/>
+</div>
+
+<div class="form-group">
+<label htmlFor="Player Name"><h1>Player Name</h1></label>
+<input type="text" className="form-control" id="Player Name" placeholder="Player Name"/>
+</div>
+<div class="form-group">
+<label htmlFor="Player Password"><h1>Player Password</h1></label>
+<input type="password" className="form-control" id="Player Password" placeholder="Password"/>
+</div>
+<div class="form-group">
+<label htmlFor="expansion"><h1>Expansion</h1></label>
+<input type="password" className="form-control" id="expansion" placeholder="expansion"/>
+</div></div>
+
+
+
+</form>  
+
+</div> </div>
+
+
+</div> }
+
+ return  <div className='MatchInfo'>  
+ <div >
+    
+  <div class="flex-container bg-light p-2 text-dark bg-opacity-75"><h1>Question:{question}</h1>  </div>
+  <div class="flex-container bg-light p-2 text-dark bg-opacity-75" ><h1>{judge} </h1>  </div> 
+ <Answer  handleAnswer={handleAnswer}/>
+<div class="flex-container"> <table  className='table'> 
+   
+
+   <thead>
+     <tr>
+   <th> <div class="bg-light p-2 text-dark bg-opacity-75">Player</div>
+ </th>
+   <th><div class="bg-light p-2 text-dark bg-opacity-75">Score</div></th>
+   <th><div class="bg-light p-2 text-dark bg-opacity-75">Answer</div></th>
+   <th></th>
+  
+   
+     </tr>
+   </thead>
+   <tbody> 
+         
+    {players.map( player =>( 
+     
+     
+   
+   <tr key = {player.name} >
+   <td> <div class="bg-light p-2 text-dark bg-opacity-75">{player.name } </div></td>
+   <td> <div class="bg-light p-2 text-dark bg-opacity-75">{player.points }</div> </td>
+   <td> <div class="bg-light p-2 text-dark bg-opacity-75">{player.response }</div></td>  
+   <td>  <button 
+   style={{color:"white" , backgroundColor: "red" }}
+   onClick={()=>{handleVote2(player.name)}}     
+   className="btn btn-danger btn-sm"> Upvote </button> </td>
+   </tr>
+     ))
+   }
+   </tbody>
+   
+    </table></div>
+   </div> 
+
+ <div> 
+  </div>     
+ 
+ </div>}
+
+
   return (
 
-    <div className="App">
-      
-             <h1> Teodorant Insanity Topic Simulator </h1>   
-
-          <img src='https://i.imgur.com/91gg3HR.png'/>
-      
-     <div>  
-     <div class>     <div><h1>Question:</h1> {question} </div>     <div><h1>Judge:</h1>  {judge} </div> 
-
-     
-
-     
-
-     </div>
-     
-     <div class="flex-container">  
-       
-     <div>
-       <table className='table'> 
-  
-    <thead>
-      <tr>
-    <th>Player</th>
-    <th>Score</th>
-    <th>Answer</th>
-    <th></th>
-    <th></th>
-
-      </tr>
-    </thead>
-    <tbody> 
-      {console.log(players)}      
-     { 
-     players.map( player =>( 
-      
-      
-    
-    <tr key = {player.name} >
-    <td> {player.name } </td>
-    <td> {player.points } </td>
-    <td> {player.response }</td>  
-    <td>  <button onClick={()=>{handleVote2(player.name)}} classname="btn btn-danger btn-sm"> Upvote </button> </td>
-    </tr>
-      ))
-    }
-    </tbody>
-    
-     </table> </div> 
-
-     <div> <form>
-  <div class="form-group">
-    <label htmlFor="Answer">  <h1> Answer </h1>  </label>
-    <input type="text" className="form-control" id="Answer" />
-     </div>
-
-
-  
-
-  <button 
-
-  onClick={handleAnswer}
-  type="submit" class="btn btn-primary"> Send Answer
-  </button>
-</form>  </div>     
-     
-       
-   <form>
-
-
-  
-
-  <div class="form-group">
-    <h1> Join/Create Game </h1>
-    
-
-<button 
-  onClick={handlesetPlayerInfo}
-  type="submit" class="btn btn-primary">Set Player and Game info</button>
-  <button 
-  onClick={handlejOin}
-  type="submit" class="btn btn-primary">Join game</button>
-  <button 
-  onClick={handlecReate}
-  type="submit" class="btn btn-primary">Create game</button>
-<button 
-  onClick={handlepaloki1}
-  type="submit" class="btn btn-primary">Super Secret Action</button>
-
-<label htmlFor="GAMEID"><h1>GAMEID</h1></label>
-<input type="password" className="form-control" id="GAMEID" />
-     </div>
-
-
-
-  <div class="form-group">
-    <label htmlFor="Game Password"><h1>Game Password</h1></label>
-    <input type="password" className="form-control" id="Game Password" placeholder="Game Password"/>
-  </div>
-
-  <div class="form-group">
-    <label htmlFor="Player Name"><h1>Player Name</h1></label>
-    <input type="text" className="form-control" id="Player Name" placeholder="Player Name"/>
-  </div>
-  <div class="form-group">
-    <label htmlFor="Player Password"><h1>Player Password</h1></label>
-    <input type="password" className="form-control" id="Player Password" placeholder="Password"/>
-  </div>
-  <div class="form-group">
-    <label htmlFor="expansion"><h1>Expansion</h1></label>
-    <input type="password" className="form-control" id="expansion" placeholder="expansion"/>
-  </div>
-  
-</form>  
-    
-
-
-
-
-
-
-
-    
-     
-     
-     
-     
+   <Display/>
       
 
-            
-
-         
-
-  </div> </div>
-  
-  <div> <img
-       src='https://images.saatchiart.com/saatchi/1250137/art/6241761/5311535-HSC00002-7.jpg'/>{gameid} {gamepassword} {playername} {playerpassword}{text2}  </div>
-   </div>
-  );}
+  ) }
 
 
 export default App;
